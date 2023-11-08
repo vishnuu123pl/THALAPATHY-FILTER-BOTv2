2a48d8f12d864de3586c21174d5ed28172201719
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-11-09 04:10:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -223,7 +223,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ :<code> {file_name} </code>""" 
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ :<code> {file_name} </code></b>""" 
 
     IMDB_TEMPLATE_TXT = """
     <b>🏷 Title</b>: <a href={url}>{title}</a>"""
