3a3c4a6cbf08e334d3e6d6206049196c2b843fa6
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-11-18 02:10:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1931,6 +1931,7 @@ async def auto_filter(client, msg, spoll=False):
             cap+="<b><u>🍿 Your Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+        
         if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
